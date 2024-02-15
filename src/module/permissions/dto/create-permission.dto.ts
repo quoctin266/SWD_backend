c4493421de072cde3e39e19beb2127381ad15221
hoanganh-luong2906 <131256206+hoanganh-luong2906@users.noreply.github.com>
2024-02-15 17:42:33 +0700

    Permission functions (#5)

--- src/module/permissions/dto/create-permission.dto.ts
+++ src/module/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,58 @@
-export class CreatePermissionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePermissionDto {
+  @IsNotEmpty()
+  @IsArray()
+  @ApiProperty({
+    example: ['ADMIN', 'USER'],
+    default: null,
+    type: String,
+  })
+  roles: String[];
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @Matches(/^[A-Z_0-9]+$/, {
+    message: 'Name must be uppercase spaced by underscore',
+  })
+  @ApiProperty({
+    example: 'USER_CREATE',
+    default: null,
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 'Permission to create a User',
+    default: null,
+  })
+  @IsOptional()
+  description: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'api/v1/users',
+    default: null,
+  })
+  @Matches(/^api\/v1/, { message: 'API must starts with api/v1/...' })
+  apiPath: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'POST',
+    default: null,
+  })
+  @Matches(/^(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS)$/i, {
+    message:
+      'Method must be GET | POST | PUT | DELETE | PATCH | HEAD | OPTIONS',
+  })
+  method: string;
+}
