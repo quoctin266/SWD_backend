37c6aaa3dfb2d63eeb3307840d47e916456a3a4d
Quoc Tin <tin.9a1.dt@gmail.com>
2024-02-19 11:29:59 +0700

    seed role and area

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,25 +1,11 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { SportType } from 'src/module/sport-types/entities/sport-type.entity';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { runSeeders } from 'typeorm-extension';
 
 @Injectable()
 export class DatabasesService implements OnModuleInit {
-  private readonly logger = new Logger(DatabasesService.name);
-
-  constructor(
-    @InjectRepository(SportType)
-    private sportTypesRepository: Repository<SportType>,
-    private dataSource: DataSource,
-  ) {}
+  constructor(private dataSource: DataSource) {}
   async onModuleInit() {
-    // change this later
-    const count = (await this.sportTypesRepository.find()).length;
-
-    if (count === 0) {
-      await runSeeders(this.dataSource);
-      this.logger.log('Run seeding complete');
-    } else this.logger.log('No seeding were run');
+    await runSeeders(this.dataSource);
   }
 }
