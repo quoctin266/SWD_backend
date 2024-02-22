d400887169b4325437b7e9a144b2d096db960956
quoctin266 <112544599+quoctin266@users.noreply.github.com>
2024-02-22 11:13:46 +0700

    fix event seeding bug (#10)
    * add like info
    
    * add common club
    
    * export swagger
    
    * auth module
    
    * sport type seeding
    
    * seed role and area
    
    * prepare script for deploy
    
    * seeding users, areas and clubs
    
    * google auth api
    
    * event seeding
    
    * fix event seeding bug

--- src/database/seeds/06_event.seed.ts
+++ src/database/seeds/06_event.seed.ts
@@ -4,6 +4,7 @@ import { Logger } from '@nestjs/common';
 import { Event } from 'src/module/events/entities/event.entity';
 import { eventData } from '../jsonData/event';
 import { Club } from 'src/module/clubs/entities/club.entity';
+import _ from 'lodash';
 
 export class EventSeeder implements Seeder {
   private readonly logger = new Logger(EventSeeder.name);
@@ -13,17 +14,22 @@ export class EventSeeder implements Seeder {
     const eventRepository = dataSource.getRepository(Event);
     const clubRepository = dataSource.getRepository(Club);
 
-    const clubsList = await clubRepository.find();
+    const clubsList = await clubRepository.find({ where: { isCommon: false } });
 
     const count = (await eventRepository.find()).length;
     if (count === 0) {
       const events = eventData.map((event) => {
-        const club = clubsList.find((item) => item.name === event.club);
+        // clubs that has sport type matching the event
+        const eventClubs = clubsList.filter(
+          (item) => item.sportType.name === event.club,
+        );
+
         return {
           ...event,
-          club,
+          club: _.sample(eventClubs),
         };
       });
+
       await eventRepository.insert(events);
       this.logger.log('Run seeding complete');
     }
