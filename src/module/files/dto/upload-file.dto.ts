644f9c2202f44bc3c717d689c66ba93a61e2b58a
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2024-02-25 13:40:11 +0700

    implements AWS file upload

---
+++ src/module/files/dto/upload-file.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+import { Express } from 'express';
+
+export class UploadFileDto {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  file: Express.Multer.File;
+
+  @IsNotEmpty()
+  folderType: string;
+
+  @IsNotEmpty()
+  folderName: string;
+}
