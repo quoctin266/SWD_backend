16b5e053badd165a9d8b2c1e454095bd4e223002
quoctin266 <112544599+quoctin266@users.noreply.github.com>
2024-03-13 14:57:36 +0700

    Seeding wallet and done application API (#20)
    * seeding wallet and done application API

---
+++ src/database/seeds/10_wallet.seed.ts
@@ -0,0 +1,53 @@
+import { DataSource } from 'typeorm';
+import { Seeder } from 'typeorm-extension';
+import { Logger } from '@nestjs/common';
+import { Wallet } from 'src/module/wallets/entities/wallet.entity';
+import { Club } from 'src/module/clubs/entities/club.entity';
+import { Member } from 'src/module/members/entities/member.entity';
+
+export class WalletSeeder implements Seeder {
+  private readonly logger = new Logger(WalletSeeder.name);
+
+  public async run(dataSource: DataSource): Promise<any> {
+    const walletsRepository = dataSource.getRepository(Wallet);
+    const clubsRepository = dataSource.getRepository(Club);
+    const membersRepository = dataSource.getRepository(Member);
+
+    const count = (await walletsRepository.find()).length;
+    if (count === 0) {
+      const allClubsList = await clubsRepository.find();
+
+      const commonClub = allClubsList.find((club) => club.isCommon === true);
+      const normalClubsList = allClubsList.filter(
+        (club) => club.isCommon === false,
+      );
+      const commonMembersList = await membersRepository.findBy({
+        club: commonClub,
+      });
+
+      // insert clubs wallet data
+      await Promise.all(
+        normalClubsList.map(async (club) => {
+          // random 50 - 150 point
+          const balance = Math.floor(Math.random() * (150 - 50 + 1) + 50);
+          return walletsRepository.insert({ balance, club });
+        }),
+      );
+
+      // insert members wallet data
+      await Promise.all(
+        commonMembersList.map(async (member) => {
+          // random 0 - 50 point
+          const balance = Math.floor(Math.random() * (50 - 0 + 1) + 0);
+          return walletsRepository.insert({
+            balance,
+            member,
+            club: commonClub,
+          });
+        }),
+      );
+
+      this.logger.log('Run seeding complete');
+    }
+  }
+}
--- src/module/applications/applications.controller.ts
+++ src/module/applications/applications.controller.ts
@@ -6,11 +6,21 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
 import { CreateApplicationDto } from './dto/create-application.dto';
 import { UpdateApplicationDto } from './dto/update-application.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from '../users/dto/users.dto';
+import {
+  CREATE_APPLICATION,
+  GET_APPLICATIONS,
+  GET_APPLICATION_DETAIL,
+  UPDATE_APPLICATION,
+} from 'src/util/message';
+import { ApplicationFilterDto } from './dto/filter-application.dto';
 
 @ApiTags('applications')
 @Controller('applications')
@@ -18,30 +28,39 @@ export class ApplicationsController {
   constructor(private readonly applicationsService: ApplicationsService) {}
 
   @Post()
-  create(@Body() createApplicationDto: CreateApplicationDto) {
-    return this.applicationsService.create(createApplicationDto);
+  @ResponseMessage(CREATE_APPLICATION)
+  create(
+    @Body() createApplicationDto: CreateApplicationDto,
+    @User() user: IUser,
+  ) {
+    const { slotId } = createApplicationDto;
+    return this.applicationsService.create(user.id, slotId);
   }
 
   @Get()
-  findList() {
-    return this.applicationsService.findList();
+  @ResponseMessage(GET_APPLICATIONS)
+  findList(@Query() query: ApplicationFilterDto) {
+    return this.applicationsService.findList(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ResponseMessage(GET_APPLICATION_DETAIL)
+  findOne(@Param('id') id: number) {
     return this.applicationsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ResponseMessage(UPDATE_APPLICATION)
   update(
-    @Param('id') id: string,
+    @Param('id') id: number,
     @Body() updateApplicationDto: UpdateApplicationDto,
   ) {
-    return this.applicationsService.update(+id, updateApplicationDto);
+    const { status } = updateApplicationDto;
+    return this.applicationsService.update(+id, status);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.applicationsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.applicationsService.remove(+id);
+  // }
 }
