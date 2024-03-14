643c9c0d3c7f15800ee6ad3fade72949f46877a8
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2024-03-14 22:28:36 +0700

    transaction API

--- src/module/transactions/transactions.service.ts
+++ src/module/transactions/transactions.service.ts
@@ -1,26 +1,147 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IsNull, Repository } from 'typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Wallet } from '../wallets/entities/wallet.entity';
+import { Application } from '../applications/entities/application.entity';
+import { NOTFOUND_TRANSACTION, NOTFOUND_WALLET } from 'src/util/message';
+import { TransactionFilterDto } from './dto/filter-transaction.dto';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionsRepository: Repository<Transaction>,
+    @InjectRepository(Wallet)
+    private walletsRepository: Repository<Wallet>,
+    @InjectRepository(Application)
+    private applicationsRepository: Repository<Application>,
+  ) {}
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const { applicationId, senderId, receiverId, amount, description } =
+      createTransactionDto;
+
+    const application = applicationId
+      ? await this.applicationsRepository.findOneBy({
+          id: applicationId,
+        })
+      : null;
+
+    const sender = await this.walletsRepository.findOneBy({ id: senderId });
+    if (!sender) throw new BadRequestException(NOTFOUND_WALLET);
+
+    const receiver = await this.walletsRepository.findOneBy({ id: receiverId });
+    if (!receiver) throw new BadRequestException(NOTFOUND_WALLET);
+
+    // transfer points from sender wallet to receiver wallet
+    await this.walletsRepository.update(senderId, {
+      balance: sender.balance - amount,
+    });
+    await this.walletsRepository.update(receiverId, {
+      balance: receiver.balance + amount,
+    });
+
+    // update application status to completed
+    if (applicationId)
+      await this.applicationsRepository.update(applicationId, {
+        status: 'COMPLETED',
+      });
+
+    const result = await this.transactionsRepository.insert({
+      amount,
+      description,
+      sender,
+      receiver,
+      application,
+    });
+
+    return result.generatedMaps[0];
   }
 
-  findList() {
-    return `This action returns all transactions`;
+  async findList(queryObj: TransactionFilterDto) {
+    const { sortBy, sortDescending, pageSize, current } = queryObj;
+
+    const defaultLimit = pageSize ? pageSize : 10;
+    const defaultPage = current ? current : 1;
+    const offset = (defaultPage - 1) * defaultLimit;
+
+    const query = this.transactionsRepository
+      .createQueryBuilder('transaction')
+      .leftJoinAndSelect('transaction.sender', 'wallet')
+      .leftJoinAndSelect('transaction.receiver', 'walletAlias')
+      .leftJoinAndSelect('transaction.application', 'application')
+      .leftJoinAndSelect('wallet.member', 'member')
+      .leftJoinAndSelect('wallet.club', 'club')
+      .leftJoinAndSelect('walletAlias.member', 'memberAlias')
+      .leftJoinAndSelect('walletAlias.club', 'clubAlias')
+      .leftJoinAndSelect('member.user', 'user')
+      .leftJoinAndSelect('memberAlias.user', 'userAlias');
+
+    const totalItems = (await query.getMany()).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const sortableCriterias = ['createdAt'];
+
+    const result = await query
+      .orderBy(
+        sortableCriterias.includes(sortBy) ? `transaction.${sortBy}` : '',
+        sortDescending ? 'DESC' : 'ASC',
+      )
+      .offset(offset)
+      .limit(defaultLimit)
+      .getMany();
+
+    const data = result.map((transaction) => {
+      return {
+        ...transaction,
+        sender: transaction.sender.member
+          ? transaction.sender.member.user.username
+          : transaction.sender.club.name,
+        receiver: transaction.receiver.member
+          ? transaction.receiver.member.user.username
+          : transaction.receiver.club.name,
+        application: transaction.application
+          ? transaction.application.id
+          : null,
+      };
+    });
+
+    return {
+      currentPage: defaultPage,
+      totalPages: totalPages,
+      pageSize: defaultLimit,
+      totalItems: totalItems,
+      items: data,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  async findOne(id: number) {
+    const transaction = await this.transactionsRepository.findOneBy({ id });
+    if (!transaction) throw new BadRequestException(NOTFOUND_TRANSACTION);
+
+    return {
+      ...transaction,
+      sender: transaction.sender.member
+        ? transaction.sender.member.user.username
+        : transaction.sender.club.name,
+      receiver: transaction.receiver.member
+        ? transaction.receiver.member.user.username
+        : transaction.receiver.club.name,
+      application: transaction.application ? transaction.application.id : null,
+    };
   }
 
   update(id: number, updateTransactionDto: UpdateTransactionDto) {
     return `This action updates a #${id} transaction`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async remove(id: number) {
+    const existTransaction = await this.transactionsRepository.existsBy({ id });
+    if (!existTransaction) throw new BadRequestException(NOTFOUND_TRANSACTION);
+
+    return this.transactionsRepository.softDelete(id);
   }
 }
--- src/module/wallets/wallets.service.ts
+++ src/module/wallets/wallets.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { WalletFilterDto } from './dto/filter-wallet.dto';
+import { IsNull, Repository } from 'typeorm';
+import { Member } from '../members/entities/member.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Club } from '../clubs/entities/club.entity';
+import { Wallet } from './entities/wallet.entity';
+import { NOTFOUND_CLUB } from 'src/util/message';
 
 @Injectable()
 export class WalletsService {
+  constructor(
+    @InjectRepository(Wallet)
+    private walletsRepository: Repository<Wallet>,
+    @InjectRepository(Club)
+    private clubsRepository: Repository<Club>,
+    @InjectRepository(Member)
+    private membersRepository: Repository<Member>,
+  ) {}
+
   create(createWalletDto: CreateWalletDto) {
     return 'This action adds a new wallet';
   }
@@ -12,8 +28,32 @@ export class WalletsService {
     return `This action returns all wallets`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wallet`;
+  async findOne(queryObj: WalletFilterDto) {
+    const { memberId, clubId } = queryObj;
+
+    const club = await this.clubsRepository.findOneBy({ id: clubId });
+    if (!club) throw new BadRequestException(NOTFOUND_CLUB);
+
+    const wallet = await this.walletsRepository.findOne({
+      relations: {
+        member: true,
+        club: true,
+      },
+      where: {
+        member: {
+          id: memberId ? memberId : IsNull(),
+        },
+        club: {
+          id: clubId,
+        },
+      },
+    });
+
+    return {
+      ...wallet,
+      club: wallet.club ? wallet.club.id : null,
+      member: wallet.member ? wallet.member.id : null,
+    };
   }
 
   update(id: number, updateWalletDto: UpdateWalletDto) {
